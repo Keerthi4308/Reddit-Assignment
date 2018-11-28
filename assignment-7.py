4214d4826d642de11a498104e9ecb2241e8f8a6f
lrschaede <lrschaede@gmail.com>
2018-11-28 11:05:06 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	assignment-7.py

--- assignment-7.py
+++ assignment-7.py
@@ -2,17 +2,28 @@ import operator
 import collections
 cnt = collections.Counter()
 import json
+import pandas as pd
+from os import listdir
+from os.path import join, isfile
+import gzip
+from datetime import datetime
+import time
 
-####Q1
+#Q1 : Top 100 subreddits based on number of unique users
 
-filename = '/l/research/social-media-mining/public/RC_2015-01-random-sample-1000000.jsonlines'
+inputfile = '/l/research/social-media-mining/public/RC_2015-01-random-sample-1000000.jsonlines'
+split = '/l/research/social-media-mining/public/comments-2015-split'
+com_path = '/l/research/social-media-mining/public/reddit/comments'
+sub_path = '/l/research/social-media-mining/public/reddit/submissions'
+comments = [f for f in listdir(com_path) if isfile(join(com_path, f))]
+submissions = [f for f in listdir(sub_path) if isfile(join(sub_path, f))]
+outputfile = "reddit-statistics.csv"
+
 posts = []
-with open(filename) as fh:
+with open(inputfile) as fh:
     for line in fh:
         line_dict = json.loads(line)
         posts.append(line_dict)
-
-#print(posts[:100])
 
 top = {}
 user = {}
@@ -27,28 +38,32 @@ for i in posts:
 
 sort = sorted(top.items(), key = operator.itemgetter(1), reverse = True)
 top_100 = sort[:100]
-#print(top_100)
-#print(user['AskReddit'])
-#print(top)
 
-# ####Q2
-#
-# total = 0
-# for i in top_100:
-#     total += i[1]
-# percent = {}
-# for i in top_100:
-#     percent[i[0]] = i[1]/total
+top_100_dict = {i[0]:i[1] for i in top_100}
+dt = pd.DataFrame(list(top_100_dict.items()), columns = ['Top 100 Subreddits', 'Unique Users'])
+dt.to_csv(outputfile, sep = ' ')
 
-####Q3
+#Q2 Percentage of content for each subreddit in the top 100
 
-top_20 = sort[:20]
-top20 = 0
-for i in top_20:
-    top20 += i[1]
-#print(top20/total)
+content = {}
+total = 0
+for i in posts:
+    if i['subreddit'] in top_100_dict:
+        if i['subreddit'] in content:
+                content[i['subreddit']] += 1
+        else:
+                content[i['subreddit']] = 1
+        total += 1
 
-##CR20
+for key, value in content.items():
+    value = value / total
+    content[key] = value
+
+dt = pd.DataFrame(list(content.items()), columns = ['Top 100 Subreddits', 'Percentage of Content'])
+dt.to_csv(outputfile, sep = ' ', mode = 'a')
+
+
+##Q4 CR20 for Top 100 Subreddits
 from nested_dict import nested_dict
 import operator
 top_100 = sort[:100]
@@ -90,5 +105,30 @@ for subreddit in subs:
     cr20 = top_20_contrib/total_comm
     print(subreddit,":", cr20)
 
+"""#Q5 Churn
+churn = {}
+for i in posts:
+    if i['subreddit'] in churn:
+        churn[i['subreddit']].append(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(i['created_utc'])))
+    else:
+        churn[i['subreddit']] = int(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(i['created_utc'])))
+print(churn)
+thing = listdir(split)
+thing = sorted(thing)
+month = []
+for i in thing:
+    month.append(i)
+jan = []
+feb = []
+for i in month:
+    url = split + '/' + i
+    with gzip.open(url) as fh:
+        for line in fh:
+            line_dict = json.loads(line)
+            if '2015-01' in time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(line_dict['created_utc'])):
+                jan.append(line_dict)
+            elif '2015-02' in time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(line_dict['created_utc'])):
+                feb.append(line_dict)
+print(jan[0], feb[0])"""
 
 
